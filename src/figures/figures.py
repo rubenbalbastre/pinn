1856b4f5131551cf049ca07165562a3c9c1d4f3b
rubenbalbastre <96421412+rubenbalbastre@users.noreply.github.com>
2025-06-25 13:27:09 +0200

    fix figures axes labels plot

--- src/figures/figures.py
+++ src/figures/figures.py
@@ -3,25 +3,30 @@ import matplotlib.pyplot as plt
 
 
 # Example usage
-def plot_sample(sample, u_xt_pred):
+def plot_sample(sample, u_xt_pred, xt_pred_mesh):
     
+    x = sample['x'].detach().numpy()
+    t = sample['t'].detach().numpy()
     u_xt = sample["u_xt"].detach().numpy()
+
+    x_pred = xt_pred_mesh['x'].detach().numpy()
+    t_pred = xt_pred_mesh['t'].detach().numpy()
     u_xt_pred = u_xt_pred.reshape(sample["nx"], sample["nt"]).detach().numpy()
 
     # Plot
     fig, ax = plt.subplots(figsize=(24, 4), nrows=1, ncols=3)
 
-    im0 = ax[0].imshow(u_xt, aspect="auto", origin="lower", extent=[0, 1, 0, 0.1])
+    im0 = ax[0].imshow(u_xt, aspect="auto", origin="lower", extent=[x.min(), x.max(), t.min(), t.max()])
     fig.colorbar(im0, ax=ax[0], label="Temperature")
     ax[0].set_xlabel("x")
     ax[0].set_ylabel("Time")
 
-    im1 = ax[1].imshow(u_xt_pred, aspect="auto", origin="lower", extent=[0, 1, 0, 0.1])
+    im1 = ax[1].imshow(u_xt_pred, aspect="auto", origin="lower", extent=[x_pred.min(), x_pred.max(), t_pred.min(), t_pred.max()])
     fig.colorbar(im1, ax=ax[1], label="Temperature NN")
     ax[1].set_xlabel("x")
     ax[1].set_ylabel("Time")
 
-    im2 = ax[2].imshow(np.abs(u_xt - u_xt_pred), aspect="auto", origin="lower", extent=[0, 1, 0, 0.1])
+    im2 = ax[2].imshow(np.abs(u_xt - u_xt_pred), aspect="auto", origin="lower", extent=[x_pred.min(), x_pred.max(), t_pred.min(), t_pred.max()])
     fig.colorbar(im2, ax=ax[2], label="Temperature Absolute error")
     ax[2].set_xlabel("x")
     ax[2].set_ylabel("Time")
@@ -29,3 +34,4 @@ def plot_sample(sample, u_xt_pred):
     fig.suptitle(f"{sample["u_type_txt"].capitalize()} (physical coefficient: {sample['kind']})")
 
     plt.show()
+    
