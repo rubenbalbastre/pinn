fix t range -> this causes pinn to be difficult to solved. maybe normalizing input and outputs is better

src/data_generator/mesh_grid.py
@@ -11,10 +11,10 @@ def generate_mesh_grid(alpha_x, nx=100, nt=100, L=1.0, T=0.1):
     dt = min(T / nt, dt_stable * 0.9)  # reduce a bit below the limit
 
     x = torch.linspace(0, L, nx)
-    t = torch.arange(0, nt, dtype=torch.float32)
+    t = torch.linspace(0, T, nt)
 
     # create input for u_network
-    X, T = torch.meshgrid(x, t, indexing='ij') # shape [nx, nt]
-    xt = torch.stack([X.flatten(), T.flatten()], dim=1) # shape [nx*nt, 2]
+    X_mesh, T_mesh = torch.meshgrid(x, t, indexing='ij') # shape [nx, nt]
+    xt = torch.stack([X_mesh.flatten(), T_mesh.flatten()], dim=1) # shape [nx*nt, 2]
 
-    return {"xt": xt, "x": x, "t": t, "nx": nx, "nt": nt, "dt": dt, "dx": dx}
+    return {"xt": xt, "x": x, "t": t, "nx": nx, "nt": nt, "dt": dt, "dx": dx, "L": L, "T": T}